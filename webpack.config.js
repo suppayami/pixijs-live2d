d32455003fa5311639747b42ecf21e7c9ffd9e8e
Nguyen Tuan Cuong <cuong.nguyentuan@digia.com>
2020-10-21 17:41:54 +0300

    opacity

--- webpack.config.js
+++ webpack.config.js
@@ -24,23 +24,23 @@ module.exports = [
         },
         externals: ['fs'],
     },
-    // {
-    //     name: 'browser',
-    //     mode: 'production',
-    //     entry: './src/pixi_live2d.ts',
-    //     devtool: 'source-map',
-    //     plugins: [new CleanWebpackPlugin()],
-    //     output: {
-    //         filename: 'pixi_live2d.browser.js',
-    //         path: path.resolve(__dirname, 'dist'),
-    //         library: ['PIXI', 'live2d'],
-    //         libraryTarget: 'umd',
-    //     },
-    //     externals: {
-    //         fs: 'fs',
-    //         'pixi.js': 'PIXI',
-    //     },
-    // },
+    {
+        name: 'browser',
+        mode: 'production',
+        entry: './src/pixi_live2d.ts',
+        devtool: 'source-map',
+        plugins: [new CleanWebpackPlugin()],
+        output: {
+            filename: 'pixi_live2d.browser.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: ['PIXI', 'live2d'],
+            libraryTarget: 'umd',
+        },
+        externals: {
+            fs: 'fs',
+            'pixi.js': 'PIXI',
+        },
+    },
 ].map((config) => ({
     ...config,
     module: {
